e586bfd727ab826da98bd3a601b89c02cdc170e0
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 20:44:17 +0900

    feat: solve exercism chessboard

--- exercism/chessboard/lib/chessboard.ex
+++ exercism/chessboard/lib/chessboard.ex
@@ -1,17 +1,17 @@
 defmodule Chessboard do
   def rank_range do
-    # Please implement the rank_range/0 function
+    1..8
   end
 
   def file_range do
-    # Please implement the file_range/0 function
+    ?A..?H
   end
 
   def ranks do
-    # Please implement the ranks/0 function
+    rank_range() |> Enum.to_list()
   end
 
   def files do
-    # Please implement the files/0 function
+    file_range() |> Enum.map(&to_string([&1]))
   end
 end
