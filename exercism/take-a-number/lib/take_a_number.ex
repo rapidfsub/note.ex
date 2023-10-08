674d6b1a7b67add28983ab8246531c1cab4edd0f
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 14:23:00 +0900

    feat: solve exercism take a number

--- exercism/take-a-number/lib/take_a_number.ex
+++ exercism/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,24 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(fn -> do_start(0) end)
+  end
+
+  defp do_start(state) do
+    receive do
+      {:report_state, pid} ->
+        send(pid, state)
+        do_start(state)
+
+      {:take_a_number, pid} ->
+        state = state + 1
+        send(pid, state)
+        do_start(state)
+
+      :stop ->
+        nil
+
+      _ ->
+        do_start(state)
+    end
   end
 end
