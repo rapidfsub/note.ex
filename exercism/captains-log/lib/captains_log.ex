56b4717fefcbf215781c9b5756a33e8f6c5596a7
Minsub Kim <rapidfsub@gmail.com>
2023-10-09 02:12:09 +0900

    feat: solve exercism captains log

--- exercism/captains-log/lib/captains_log.ex
+++ exercism/captains-log/lib/captains_log.ex
@@ -1,19 +1,23 @@
 defmodule CaptainsLog do
-  @planetary_classes ["D", "H", "J", "K", "L", "M", "N", "R", "T", "Y"]
+  @planetary_classes ~w[D H J K L M N R T Y]
 
   def random_planet_class() do
-    # Please implement the random_planet_class/0 function
+    Enum.random(@planetary_classes)
   end
 
   def random_ship_registry_number() do
-    # Please implement the random_ship_registry_number/0 function
+    "NCC-#{Enum.random(1000..9999)}"
   end
 
   def random_stardate() do
-    # Please implement the random_stardate/0 function
+    1000 * :rand.uniform() + 41000
   end
 
-  def format_stardate(stardate) do
-    # Please implement the format_stardate/1 function
+  def format_stardate(stardate) when is_float(stardate) do
+    Float.round(stardate, 1) |> to_string()
+  end
+
+  def format_stardate(_stardate) do
+    raise ArgumentError
   end
 end
