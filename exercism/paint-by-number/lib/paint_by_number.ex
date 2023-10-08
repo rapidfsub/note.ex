b3affc45456c21dbbdc8a68af7c1592c36baea18
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 15:47:39 +0900

    feat: solve exercism paint by number

--- exercism/paint-by-number/lib/paint_by_number.ex
+++ exercism/paint-by-number/lib/paint_by_number.ex
@@ -1,29 +1,45 @@
 defmodule PaintByNumber do
-  def palette_bit_size(color_count) do
-    # Please implement the palette_bit_size/1 function
+  def palette_bit_size(color_count) when color_count > 2 do
+    ceil(color_count / 2)
+    |> palette_bit_size()
+    |> Kernel.+(1)
+  end
+
+  def palette_bit_size(color_count) when color_count > 0 do
+    1
   end
 
   def empty_picture() do
-    # Please implement the empty_picture/0 function
+    <<>>
   end
 
   def test_picture() do
-    # Please implement the test_picture/0 function
+    <<0::2, 1::2, 2::2, 3::2>>
   end
 
   def prepend_pixel(picture, color_count, pixel_color_index) do
-    # Please implement the prepend_pixel/3 function
+    <<pixel_color_index::size(palette_bit_size(color_count)), picture::bitstring>>
   end
 
   def get_first_pixel(picture, color_count) do
-    # Please implement the get_first_pixel/2 function
+    split_first_pixel(picture, color_count) |> elem(0)
   end
 
   def drop_first_pixel(picture, color_count) do
-    # Please implement the drop_first_pixel/2 function
+    split_first_pixel(picture, color_count) |> elem(1)
+  end
+
+  defp split_first_pixel(<<>>, _color_count) do
+    {nil, <<>>}
+  end
+
+  defp split_first_pixel(picture, color_count) do
+    count = palette_bit_size(color_count)
+    <<first::size(^count), rest::bitstring>> = picture
+    {first, rest}
   end
 
   def concat_pictures(picture1, picture2) do
-    # Please implement the concat_pictures/2 function
+    <<picture1::bitstring, picture2::bitstring>>
   end
 end
