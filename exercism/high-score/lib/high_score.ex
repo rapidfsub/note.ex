66f93c13d642526b360c82567d6f16127dc159bb
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 12:34:56 +0900

    refactor: use module attribute

--- exercism/high-score/lib/high_score.ex
+++ exercism/high-score/lib/high_score.ex
@@ -1,9 +1,11 @@
 defmodule HighScore do
+  @initial_score 0
+
   def new() do
     %{}
   end
 
-  def add_player(scores, name, score \\ 0) do
+  def add_player(scores, name, score \\ @initial_score) do
     Map.put(scores, name, score)
   end
 
@@ -12,7 +14,7 @@ defmodule HighScore do
   end
 
   def reset_score(scores, name) do
-    Map.put(scores, name, 0)
+    Map.put(scores, name, @initial_score)
   end
 
   def update_score(scores, name, score) do
