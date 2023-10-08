948ffd04d132e5574f16dc4e4039aef9012a710d
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 17:37:48 +0900

    feat: solve exercism basketball website

--- exercism/basketball-website/lib/basketball_website.ex
+++ exercism/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,17 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    do_extract_from_path(data, keys(path))
   end
 
+  defp keys(path) do
+    String.split(path, ".")
+  end
+
+  defp do_extract_from_path(nil, _keys), do: nil
+  defp do_extract_from_path(data, []), do: data
+  defp do_extract_from_path(data, [key | rest]), do: do_extract_from_path(data[key], rest)
+
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    get_in(data, keys(path))
   end
 end
