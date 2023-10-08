d8d25becff09d288aa44831dc3e62c6ee6f0cb46
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 20:08:03 +0900

    feat: solve exercism top secret

--- exercism/top-secret/lib/top_secret.ex
+++ exercism/top-secret/lib/top_secret.ex
@@ -1,13 +1,26 @@
 defmodule TopSecret do
   def to_ast(string) do
-    # Please implement the to_ast/1 function
+    Code.string_to_quoted!(string)
+  end
+
+  def decode_secret_message_part({value, _, nodes} = ast, acc) when value in [:def, :defp] do
+    {name, args} =
+      case nodes do
+        [{:when, _, [{name, _, args} | _]} | _] -> {name, args}
+        [{name, _, args} | _] -> {name, args}
+      end
+
+    name = to_string(name)
+    arity = length(args || [])
+    {ast, [binary_part(name, 0, arity) | acc]}
   end
 
   def decode_secret_message_part(ast, acc) do
-    # Please implement the decode_secret_message_part/2 function
+    {ast, acc}
   end
 
   def decode_secret_message(string) do
-    # Please implement the decode_secret_message/1 function
+    {_ast, acc} = Macro.prewalk(to_ast(string), [], &decode_secret_message_part/2)
+    Enum.reverse(acc) |> to_string()
   end
 end
