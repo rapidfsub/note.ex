c0b7b1c125acb563f4e2c4c7a09de5a4e67bf8d9
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 21:51:28 +0900

    feat: solve exercism dna encoding

--- exercism/dna-encoding/lib/dna.ex
+++ exercism/dna-encoding/lib/dna.ex
@@ -1,17 +1,19 @@
 defmodule DNA do
-  def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
-  end
+  def encode_nucleotide(?\s), do: 0b0000
+  def encode_nucleotide(?A), do: 0b0001
+  def encode_nucleotide(?C), do: 0b0010
+  def encode_nucleotide(?G), do: 0b0100
+  def encode_nucleotide(?T), do: 0b1000
 
-  def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
-  end
+  def decode_nucleotide(0b0000), do: ?\s
+  def decode_nucleotide(0b0001), do: ?A
+  def decode_nucleotide(0b0010), do: ?C
+  def decode_nucleotide(0b0100), do: ?G
+  def decode_nucleotide(0b1000), do: ?T
 
-  def encode(dna) do
-    # Please implement the encode/1 function
-  end
+  def encode([]), do: <<>>
+  def encode([nucleotide | rest]), do: <<encode_nucleotide(nucleotide)::4, encode(rest)::bits>>
 
-  def decode(dna) do
-    # Please implement the decode/1 function
-  end
+  def decode(<<>>), do: []
+  def decode(<<code::4, rest::bits>>), do: [decode_nucleotide(code) | decode(rest)]
 end
