7a51c358ba1ea31581d889a94527ecd18908f96e
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 13:56:38 +0900

    feat: solve exercism name badge

--- exercism/name-badge/lib/name_badge.ex
+++ exercism/name-badge/lib/name_badge.ex
@@ -1,5 +1,15 @@
 defmodule NameBadge do
   def print(id, name, department) do
-    # Please implement the print/3 function
+    department = print_department(department)
+
+    if is_nil(id) do
+      [name, department]
+    else
+      ["[#{id}]", name, department]
+    end
+    |> Enum.join(" - ")
   end
+
+  defp print_department(nil), do: "OWNER"
+  defp print_department(value), do: to_string(value) |> String.upcase()
 end
