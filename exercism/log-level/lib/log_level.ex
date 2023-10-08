7465226fdba0f3fefc3f5524ae6cb8d49dfc869c
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 11:24:21 +0900

    feat: solve exercism log level

--- exercism/log-level/lib/log_level.ex
+++ exercism/log-level/lib/log_level.ex
@@ -1,9 +1,28 @@
 defmodule LogLevel do
-  def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
-  end
+  def to_label(0, false), do: :trace
+  def to_label(1, _legacy?), do: :debug
+  def to_label(2, _legacy?), do: :info
+  def to_label(3, _legacy?), do: :warning
+  def to_label(4, _legacy?), do: :error
+  def to_label(5, false), do: :fatal
+  def to_label(_level, _legacy?), do: :unknown
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+
+    cond do
+      label in [:error, :fatal] ->
+        :ops
+
+      label == :unknown ->
+        if legacy? do
+          :dev1
+        else
+          :dev2
+        end
+
+      true ->
+        false
+    end
   end
 end
