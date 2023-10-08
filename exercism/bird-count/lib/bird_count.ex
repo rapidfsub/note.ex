d586baa53f62498ef9b46dc6b24d254cc802576a
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 12:25:00 +0900

    feat: solve exercism bird count

--- exercism/bird-count/lib/bird_count.ex
+++ exercism/bird-count/lib/bird_count.ex
@@ -1,21 +1,25 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
-  end
+  def today([]), do: nil
+  def today([result | _]), do: result
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-  end
+  def increment_day_count([]), do: [1]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _tail]), do: true
+  def has_day_without_birds?([_head | tail]), do: has_day_without_birds?(tail)
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
+  def total([]), do: 0
+  def total([head | tail]), do: head + total(tail)
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
+  def busy_days([]), do: 0
+  def busy_days([head | tail]), do: busy_day(head) + busy_days(tail)
+
+  defp busy_day(birds) do
+    if birds < 5 do
+      0
+    else
+      1
+    end
   end
 end
