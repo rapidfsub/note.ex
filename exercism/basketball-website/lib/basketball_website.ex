a228d1960f1ec98642f5c771b7725764c88b620b
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 17:41:29 +0900

    refactor: use Enum.reduce_while/3

--- exercism/basketball-website/lib/basketball_website.ex
+++ exercism/basketball-website/lib/basketball_website.ex
@@ -1,15 +1,18 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    do_extract_from_path(data, keys(path))
+    path
+    |> keys()
+    |> Enum.reduce_while(data, fn key, data ->
+      case data[key] do
+        nil -> {:halt, nil}
+        data -> {:cont, data}
+      end
+    end)
   end
 
   defp keys(path) do
     String.split(path, ".")
   end
-
-  defp do_extract_from_path(nil, _keys), do: nil
-  defp do_extract_from_path(data, []), do: data
-  defp do_extract_from_path(data, [key | rest]), do: do_extract_from_path(data[key], rest)
 
   def get_in_path(data, path) do
     get_in(data, keys(path))
