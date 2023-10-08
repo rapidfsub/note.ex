2169ab20f92c5d7588744ebbb384689e87ee31ca
Minsub Kim <rapidfsub@gmail.com>
2023-10-08 11:04:07 +0900

    feat: solve exercism freelancer rates

--- exercism/freelancer-rates/lib/freelancer_rates.ex
+++ exercism/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,23 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8.0
+  end
+
+  defp daily_rate(hourly_rate, discount) do
+    hourly_rate
+    |> daily_rate()
+    |> apply_discount(discount)
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * (100 - discount) / 100
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    ceil(daily_rate(hourly_rate, discount) * 22.0)
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    Float.floor(budget / daily_rate(hourly_rate, discount), 1)
   end
 end
