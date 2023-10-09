3ea966da4827cdde054d1450e5dff82146ab7925
Minsub Kim <rapidfsub@gmail.com>
2023-10-09 09:38:29 +0900

    feat: solve exercism stack underflow

--- exercism/stack-underflow/lib/rpn_calculator/exception.ex
+++ exercism/stack-underflow/lib/rpn_calculator/exception.ex
@@ -1,5 +1,33 @@
 defmodule RPNCalculator.Exception do
-  # Please implement DivisionByZeroError here.
+  defmodule DivisionByZeroError do
+    defexception message: "division by zero occurred"
+  end
 
-  # Please implement StackUnderflowError here.
+  defmodule StackUnderflowError do
+    defexception message: "stack underflow occurred"
+
+    def exception(context) when is_binary(context) do
+      %__MODULE__{message: "stack underflow occurred, context: #{context}"}
+    end
+
+    def exception([]) do
+      %__MODULE__{}
+    end
+  end
+
+  def divide([0, _dividend]) do
+    raise DivisionByZeroError
+  end
+
+  def divide([divisor, dividend]) do
+    div(dividend, divisor)
+  end
+
+  def divide([_]) do
+    raise StackUnderflowError, "when dividing"
+  end
+
+  def divide([]) do
+    raise StackUnderflowError, "when dividing"
+  end
 end
