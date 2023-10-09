d07a8e56011aa98bc33fa44675bfbfb83b049694
Minsub Kim <rapidfsub@gmail.com>
2023-10-09 13:07:53 +0900

    feat: solve exercism rpn calculator inspection

--- exercism/rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
+++ exercism/rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
@@ -1,17 +1,36 @@
 defmodule RPNCalculatorInspection do
   def start_reliability_check(calculator, input) do
-    # Please implement the start_reliability_check/2 function
+    %{pid: spawn_link(fn -> calculator.(input) end), input: input}
   end
 
   def await_reliability_check_result(%{pid: pid, input: input}, results) do
-    # Please implement the await_reliability_check_result/2 function
+    receive do
+      {:EXIT, ^pid, :normal} -> Map.put(results, input, :ok)
+      {:EXIT, ^pid, _reason} -> Map.put(results, input, :error)
+    after
+      100 -> Map.put(results, input, :timeout)
+    end
   end
 
   def reliability_check(calculator, inputs) do
-    # Please implement the reliability_check/2 function
+    inputs
+    |> Enum.map(fn input ->
+      Task.async(fn ->
+        Process.flag(:trap_exit, true)
+
+        calculator
+        |> start_reliability_check(input)
+        |> await_reliability_check_result(%{})
+        |> Enum.fetch!(0)
+      end)
+    end)
+    |> Task.await_many()
+    |> Enum.into(%{})
   end
 
   def correctness_check(calculator, inputs) do
-    # Please implement the correctness_check/2 function
+    inputs
+    |> Enum.map(&Task.async(fn -> calculator.(&1) end))
+    |> Task.await_many(100)
   end
 end
