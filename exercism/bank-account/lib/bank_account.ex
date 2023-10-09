7b75033bf198a0ed236cbfb911bcb2f21554db6e
Minsub Kim <rapidfsub@gmail.com>
2023-10-10 00:28:20 +0900

    feat: solve exercism bank account

--- exercism/bank-account/lib/bank_account.ex
+++ exercism/bank-account/lib/bank_account.ex
@@ -3,6 +3,10 @@ defmodule BankAccount do
   A bank account that supports access from multiple processes.
   """
 
+  defmodule State do
+    defstruct balance: 0, closed: false
+  end
+
   @typedoc """
   An account handle.
   """
@@ -13,6 +17,8 @@ defmodule BankAccount do
   """
   @spec open() :: account
   def open() do
+    {:ok, account} = Agent.start(fn -> %State{} end)
+    account
   end
 
   @doc """
@@ -20,6 +26,7 @@ defmodule BankAccount do
   """
   @spec close(account) :: any
   def close(account) do
+    Agent.update(account, fn state -> %{state | closed: true} end)
   end
 
   @doc """
@@ -27,13 +34,25 @@ defmodule BankAccount do
   """
   @spec balance(account) :: integer | {:error, :account_closed}
   def balance(account) do
+    Agent.get(account, fn
+      %State{closed: true} -> {:error, :account_closed}
+      state -> state.balance
+    end)
   end
 
   @doc """
   Add the given amount to the account's balance.
   """
   @spec deposit(account, integer) :: :ok | {:error, :account_closed | :amount_must_be_positive}
+  def deposit(_account, amount) when amount <= 0 do
+    {:error, :amount_must_be_positive}
+  end
+
   def deposit(account, amount) do
+    Agent.get_and_update(account, fn
+      %State{closed: true} = state -> {{:error, :account_closed}, state}
+      state -> {:ok, %{state | balance: state.balance + amount}}
+    end)
   end
 
   @doc """
@@ -41,6 +60,21 @@ defmodule BankAccount do
   """
   @spec withdraw(account, integer) ::
           :ok | {:error, :account_closed | :amount_must_be_positive | :not_enough_balance}
+  def withdraw(_account, amount) when amount <= 0 do
+    {:error, :amount_must_be_positive}
+  end
+
   def withdraw(account, amount) do
+    Agent.get_and_update(account, fn
+      %State{closed: true} = state ->
+        {{:error, :account_closed}, state}
+
+      state ->
+        if state.balance < amount do
+          {{:error, :not_enough_balance}, state}
+        else
+          {:ok, %{state | balance: state.balance - amount}}
+        end
+    end)
   end
 end
