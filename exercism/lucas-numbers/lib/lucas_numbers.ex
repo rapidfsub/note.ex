009e4a7c1fbc27e77e9216051149c652d5ae659d
Minsub Kim <rapidfsub@gmail.com>
2023-10-09 10:32:58 +0900

    feat: solve exercism lucas numbers

--- exercism/lucas-numbers/lib/lucas_numbers.ex
+++ exercism/lucas-numbers/lib/lucas_numbers.ex
@@ -5,7 +5,13 @@ defmodule LucasNumbers do
 
   E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
   """
-  def generate(count) do
-    # Please implement the generate/1 function
+  def generate(count) when is_integer(count) and count > 0 do
+    {2, 1}
+    |> Stream.unfold(fn {curr, next} -> {curr, {next, curr + next}} end)
+    |> Enum.take(count)
+  end
+
+  def generate(_count) do
+    raise ArgumentError, "count must be specified as an integer >= 1"
   end
 end
