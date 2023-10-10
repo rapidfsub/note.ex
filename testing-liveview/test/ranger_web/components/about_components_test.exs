521b98ef95a165fcbea0491929e40b7c1cbeef65
Minsub Kim <rapidfsub@gmail.com>
2023-10-10 14:57:24 +0900

    test: function components

--- testing-liveview/test/ranger_web/components/about_components_test.exs
+++ testing-liveview/test/ranger_web/components/about_components_test.exs
@@ -5,4 +5,47 @@ defmodule RangerWeb.AboutComponentsTest do
   import Phoenix.LiveViewTest
 
   import RangerWeb.AboutComponents
+
+  describe "badge" do
+    test "renders content in badge (render_component)" do
+      component = render_component(&badge/1, type: "hobbit")
+      assert component =~ "hobbit"
+    end
+
+    test "renders content in badge (HEEX)" do
+      assigns = %{}
+
+      component =
+        ~H"""
+        <.badge type="hobbit" />
+        """
+        |> rendered_to_string()
+
+      assert component =~ "hobbit"
+    end
+
+    test "renders green badge for hobbits" do
+      assigns = %{}
+
+      component =
+        ~H"""
+        <.badge type="hobbit" />
+        """
+        |> rendered_to_string()
+
+      assert component =~ "bg-green"
+    end
+
+    test "renders red badge for wizards" do
+      assigns = %{}
+
+      component =
+        ~H"""
+        <.badge type="wizard" />
+        """
+        |> rendered_to_string()
+
+      assert component =~ "bg-red"
+    end
+  end
 end
