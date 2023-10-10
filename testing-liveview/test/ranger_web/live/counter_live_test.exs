1d0f80e9d43947e8430a8a4d789ace6d0107818a
Minsub Kim <rapidfsub@gmail.com>
2023-10-10 11:04:34 +0900

    test: interactions

--- testing-liveview/test/ranger_web/live/counter_live_test.exs
+++ testing-liveview/test/ranger_web/live/counter_live_test.exs
@@ -2,4 +2,34 @@ defmodule RangerWeb.CounterLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "user can increase counter", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view
+    |> element("#increment")
+    |> render_click()
+
+    assert has_element?(view, ~s"#count", "1")
+  end
+
+  test "user can increase counter (uses HTML)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    html =
+      view
+      |> element("#increment")
+      |> render_click()
+
+    assert html =~ "1"
+  end
+
+  test "user can increase counter (target event directly)", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/counter")
+
+    view
+    |> render_click("increase")
+
+    assert has_element?(view, ~s"#count", "1")
+  end
 end
