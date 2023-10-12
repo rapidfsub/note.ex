72b09f3b015aed3037cf63d79d8a36d9ee8f6bf2
Minsub Kim <rapidfsub@gmail.com>
2023-10-12 11:41:44 +0900

    feat: solve exercism clock

--- exercism/clock/lib/clock.ex
+++ exercism/clock/lib/clock.ex
@@ -7,8 +7,20 @@ defmodule Clock do
       iex> Clock.new(8, 9) |> to_string
       "08:09"
   """
-  @spec new(integer, integer) :: Clock
+  @spec new(integer, integer) :: %Clock{}
   def new(hour, minute) do
+    {carry, minute} = div_mod(minute, 60)
+    {_carry, hour} = div_mod(hour + carry, 24)
+    %Clock{hour: hour, minute: minute}
+  end
+
+  defp div_mod(divisor, dividend) do
+    division = div(divisor, dividend)
+
+    case rem(divisor, dividend) do
+      modulo when modulo < 0 -> {division - 1, modulo + dividend}
+      modulo -> {division, modulo}
+    end
   end
 
   @doc """
@@ -17,7 +29,16 @@ defmodule Clock do
       iex> Clock.new(10, 0) |> Clock.add(3) |> to_string
       "10:03"
   """
-  @spec add(Clock, integer) :: Clock
+  @spec add(%Clock{}, integer) :: %Clock{}
   def add(%Clock{hour: hour, minute: minute}, add_minute) do
+    new(hour, minute + add_minute)
+  end
+
+  defimpl String.Chars do
+    import Kernel, except: [to_string: 1]
+
+    def to_string(%Clock{hour: hour, minute: minute}) do
+      "#{String.pad_leading(Kernel.to_string(hour), 2, "0")}:#{String.pad_leading(Kernel.to_string(minute), 2, "0")}"
+    end
   end
 end
