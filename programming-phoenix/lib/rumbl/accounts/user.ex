8488ad5f504beda4e534c72dd1745c8fa5b4a72e
Minsub Kim <rapidfsub@gmail.com>
2023-10-12 16:16:15 +0900

    feat: define the user schema and migration

--- programming-phoenix/lib/rumbl/accounts/user.ex
+++ programming-phoenix/lib/rumbl/accounts/user.ex
@@ -1,3 +1,12 @@
 defmodule Rumbl.Accounts.User do
-  defstruct [:id, :name, :username]
+  import Ecto.Changeset
+
+  use Ecto.Schema
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
 end
