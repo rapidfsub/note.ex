36ac6c194b09b072e09282b17ab868b059f801eb
Minsub Kim <rapidfsub@gmail.com>
2023-10-17 18:03:00 +0900

    feat: build the api layer

--- designing-elixir-systems-with-otp/lib/mastery.ex
+++ designing-elixir-systems-with-otp/lib/mastery.ex
@@ -1,18 +1,38 @@
 defmodule Mastery do
-  @moduledoc """
-  Documentation for `Mastery`.
-  """
+  alias Mastery.Boundary.{QuizManager, QuizSession}
+  alias Mastery.Boundary.{QuizValidator, TemplateValidator}
+  alias Mastery.Core.Quiz
 
-  @doc """
-  Hello world.
+  def start_quiz_manager() do
+    GenServer.start_link(QuizManager, %{}, name: QuizManager)
+  end
 
-  ## Examples
+  def build_quiz(fields) do
+    with :ok <- QuizValidator.errors(fields),
+         :ok <- QuizManager.build_quiz(fields) do
+      :ok
+    end
+  end
 
-      iex> Mastery.hello()
-      :world
+  def add_template(title, fields) do
+    with :ok <- TemplateValidator.errors(fields),
+         :ok <- QuizManager.add_template(title, fields) do
+      :ok
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def take_quiz(title, email) do
+    with %Quiz{} = quiz <- QuizManager.lookup_quiz_by_title(title),
+         {:ok, session} <- GenServer.start_link(QuizSession, {quiz, email}) do
+      session
+    end
+  end
+
+  def select_question(session) do
+    QuizSession.select_question(session)
+  end
+
+  def answer_question(session, answer) do
+    QuizSession.answer_question(session, answer)
   end
 end
