78a4bd5d5c7d3dca53b1d3ae8fbe0e8cb67c47e6
Minsub Kim <rapidfsub@gmail.com>
2023-10-25 15:37:23 +0900

    refactor: use Macro.escape/2

--- exercism/dot-dsl/lib/dot.ex
+++ exercism/dot-dsl/lib/dot.ex
@@ -1,18 +1,21 @@
 defmodule Dot do
   defmacro graph(ast) do
-    Macro.prewalk(ast, quote(do: Graph.new()), &do_graph/2) |> elem(1)
+    ast
+    |> Macro.prewalk(Graph.new(), &do_graph/2)
+    |> elem(1)
+    |> Macro.escape()
   end
 
   defp do_graph({:graph, _, [attrs]}, acc) when is_list(attrs) do
-    {:graph, quote(do: Graph.put_attrs(unquote(acc), unquote(attrs)))}
+    {:graph, Graph.put_attrs(acc, attrs)}
   end
 
   defp do_graph({:--, _, [{from, _, nil}, {to, _, nil}]}, acc) do
-    {:edge, quote(do: Graph.add_edge(unquote(acc), unquote(from), unquote(to)))}
+    {:edge, Graph.add_edge(acc, from, to)}
   end
 
   defp do_graph({:--, _, [{from, _, nil}, {to, _, [attrs]}]}, acc) when is_list(attrs) do
-    {:edge, quote(do: Graph.add_edge(unquote(acc), unquote(from), unquote(to), unquote(attrs)))}
+    {:edge, Graph.add_edge(acc, from, to, attrs)}
   end
 
   defp do_graph([do: _] = ast, acc) do
@@ -24,12 +27,12 @@ defmodule Dot do
   end
 
   defp do_graph({id, _, nil} = ast, acc) do
-    {ast, quote(do: Graph.add_node(unquote(acc), unquote(id)))}
+    {ast, Graph.add_node(acc, id)}
   end
 
   defp do_graph({id, _, [attrs]}, acc) do
     if Keyword.keyword?(attrs) do
-      {:node, quote(do: Graph.add_node(unquote(acc), unquote(id), unquote(attrs)))}
+      {:node, Graph.add_node(acc, id, attrs)}
     else
       raise ArgumentError
     end
