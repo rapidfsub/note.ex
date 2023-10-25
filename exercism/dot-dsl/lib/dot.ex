ac001e79e372190163cbf7b1dcad84a78375be92
Minsub Kim <rapidfsub@gmail.com>
2023-10-25 15:43:35 +0900

    refactor: do refactor

--- exercism/dot-dsl/lib/dot.ex
+++ exercism/dot-dsl/lib/dot.ex
@@ -1,5 +1,5 @@
 defmodule Dot do
-  defmacro graph(ast) do
+  defmacro graph(do: ast) do
     ast
     |> Macro.prewalk(Graph.new(), &do_graph/2)
     |> elem(1)
@@ -18,31 +18,19 @@ defmodule Dot do
     {:edge, Graph.add_edge(acc, from, to, attrs)}
   end
 
-  defp do_graph([do: _] = ast, acc) do
+  defp do_graph({id, _, nil}, acc) do
+    {:node, Graph.add_node(acc, id)}
+  end
+
+  defp do_graph({id, _, [attrs]}, acc) when is_list(attrs) do
+    {:node, Graph.add_node(acc, id, attrs)}
+  end
+
+  defp do_graph(ast, acc) when is_tuple(ast) do
     {ast, acc}
   end
 
-  defp do_graph({:__block, _, _} = ast, acc) do
-    {ast, acc}
-  end
-
-  defp do_graph({id, _, nil} = ast, acc) do
-    {ast, Graph.add_node(acc, id)}
-  end
-
-  defp do_graph({id, _, [attrs]}, acc) do
-    if Keyword.keyword?(attrs) do
-      {:node, Graph.add_node(acc, id, attrs)}
-    else
-      raise ArgumentError
-    end
-  end
-
-  defp do_graph(ast, _acc) when is_integer(ast) or is_list(ast) do
+  defp do_graph(_ast, _acc) do
     raise ArgumentError
-  end
-
-  defp do_graph(ast, acc) do
-    {ast, acc}
   end
 end
