09f233e3fdab566dccfc838c8986790faacb23ee
Minsub Kim <rapidfsub@gmail.com>
2023-11-01 19:35:07 +0900

    feat: solve exercism complex numbers

--- exercism/complex-numbers/lib/complex_numbers.ex
+++ exercism/complex-numbers/lib/complex_numbers.ex
@@ -11,14 +11,16 @@ defmodule ComplexNumbers do
   Return the real part of a complex number
   """
   @spec real(a :: complex) :: float
-  def real(a) do
+  def real({real, _imag}) do
+    real
   end
 
   @doc """
   Return the imaginary part of a complex number
   """
   @spec imaginary(a :: complex) :: float
-  def imaginary(a) do
+  def imaginary({_real, imag}) do
+    imag
   end
 
   @doc """
@@ -26,6 +28,9 @@ defmodule ComplexNumbers do
   """
   @spec mul(a :: complex | float, b :: complex | float) :: complex
   def mul(a, b) do
+    {real_a, imag_a} = to_complex(a)
+    {real_b, imag_b} = to_complex(b)
+    {real_a * real_b - imag_a * imag_b, real_a * imag_b + real_b * imag_a}
   end
 
   @doc """
@@ -33,6 +38,9 @@ defmodule ComplexNumbers do
   """
   @spec add(a :: complex | float, b :: complex | float) :: complex
   def add(a, b) do
+    {real_a, imag_a} = to_complex(a)
+    {real_b, imag_b} = to_complex(b)
+    {real_a + real_b, imag_a + imag_b}
   end
 
   @doc """
@@ -40,6 +48,7 @@ defmodule ComplexNumbers do
   """
   @spec sub(a :: complex | float, b :: complex | float) :: complex
   def sub(a, b) do
+    mul(b, -1.0) |> add(a)
   end
 
   @doc """
@@ -47,26 +56,38 @@ defmodule ComplexNumbers do
   """
   @spec div(a :: complex | float, b :: complex | float) :: complex
   def div(a, b) do
+    {real_b, imag_b} = b = to_complex(b)
+
+    b
+    |> conjugate()
+    |> mul(a)
+    |> mul(1 / (real_b ** 2 + imag_b ** 2))
   end
 
   @doc """
   Absolute value of a complex number
   """
   @spec abs(a :: complex) :: float
-  def abs(a) do
+  def abs({real, imag}) do
+    :math.sqrt(real ** 2 + imag ** 2)
   end
 
   @doc """
   Conjugate of a complex number
   """
   @spec conjugate(a :: complex) :: complex
-  def conjugate(a) do
+  def conjugate({real, imag}) do
+    {real, -imag}
   end
 
   @doc """
   Exponential of a complex number
   """
   @spec exp(a :: complex) :: complex
-  def exp(a) do
+  def exp({real, imag}) do
+    :math.exp(real) |> mul({:math.cos(imag), :math.sin(imag)})
   end
+
+  defp to_complex(a) when is_number(a), do: {a, 0}
+  defp to_complex({real, imag}) when is_number(real) and is_number(imag), do: {real, imag}
 end
