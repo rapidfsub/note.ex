51db203e77bf366c669ce4fd058bbd14a05656b7
Minsub Kim <rapidfsub@gmail.com>
2023-12-20 17:08:51 +0900

    feat: solve exercism square root

--- exercism/square-root/lib/square_root.ex
+++ exercism/square-root/lib/square_root.ex
@@ -3,6 +3,17 @@ defmodule SquareRoot do
   Calculate the integer square root of a positive integer
   """
   @spec calculate(radicand :: pos_integer) :: pos_integer
-  def calculate(radicand) do
+  def calculate(radicand) when 0 < radicand do
+    calculate(radicand, 1, radicand)
+  end
+
+  defp calculate(radicand, lower, upper) do
+    result = (lower + upper) / 2
+
+    cond do
+      result * result == radicand -> result
+      result * result > radicand -> calculate(radicand, lower, result)
+      result * result < radicand -> calculate(radicand, result, upper)
+    end
   end
 end
