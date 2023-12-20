2f00f445993d7c8bda089fb3eae1b8451c5cc819
Minsub Kim <rapidfsub@gmail.com>
2023-12-20 10:48:09 +0900

    feat: solve exercism luhn

--- exercism/luhn/lib/luhn.ex
+++ exercism/luhn/lib/luhn.ex
@@ -4,5 +4,23 @@ defmodule Luhn do
   """
   @spec valid?(String.t()) :: boolean
   def valid?(number) do
+    letters = Regex.replace(~r/\s+/, number, "")
+
+    Regex.match?(~r/^\d+$/, letters) and
+      1 < byte_size(letters) and
+      letters
+      |> String.graphemes()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.reverse()
+      |> Enum.with_index()
+      |> Enum.map(fn {digit, index} ->
+        if rem(index, 2) == 0 or digit == 9 do
+          digit
+        else
+          rem(digit * 2, 9)
+        end
+      end)
+      |> Enum.sum()
+      |> rem(10) == 0
   end
 end
