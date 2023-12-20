8bb973bf4d5314a971a30b8b7fd9c25c11deba60
Minsub Kim <rapidfsub@gmail.com>
2023-12-20 12:10:22 +0900

    build: install storybook

--- loft/mix.exs
+++ loft/mix.exs
@@ -50,7 +50,8 @@ defmodule Loft.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:phoenix_storybook, "~> 0.5.0"}
     ]
   end
 
