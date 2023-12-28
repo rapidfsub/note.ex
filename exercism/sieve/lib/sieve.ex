c56b3b89a195808686c90b9b9053fef8173fbad7
Minsub Kim <rapidfsub@gmail.com>
2023-12-28 15:09:39 +0900

    feat: solve exercism sieve

--- exercism/sieve/lib/sieve.ex
+++ exercism/sieve/lib/sieve.ex
@@ -4,5 +4,13 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
+    sieve(2..limit//1)
+  end
+
+  defp sieve(numbers) do
+    case Enum.split(numbers, 1) do
+      {[], []} -> []
+      {[head], tail} -> [head | Enum.filter(tail, &(rem(&1, head) != 0)) |> sieve()]
+    end
   end
 end
