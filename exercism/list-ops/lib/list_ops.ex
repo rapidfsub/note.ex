5c6d559275a0b01baeb0bab1b3ec118d3fa9bbc3
Minsub Kim <rapidfsub@gmail.com>
2023-12-28 16:45:10 +0900

    feat: solve exercism list ops

--- exercism/list-ops/lib/list_ops.ex
+++ exercism/list-ops/lib/list_ops.ex
@@ -6,35 +6,47 @@ defmodule ListOps do
   # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-  end
+  def count(l), do: count(l, 0)
+  defp count([], result), do: result
+  defp count([_ | tail], result), do: count(tail, result + 1)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-  end
+  def reverse(l), do: reverse(l, [])
+  defp reverse([], result), do: result
+  defp reverse([head | tail], result), do: reverse(tail, [head | result])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-  end
+  def map([], _f), do: []
+  def map([head | tail], f), do: [f.(head) | map(tail, f)]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
+  def filter([], _f) do
+    []
+  end
+
+  def filter([head | tail], f) do
+    if f.(head) do
+      [head | filter(tail, f)]
+    else
+      filter(tail, f)
+    end
   end
 
   @type acc :: any
   @spec foldl(list, acc, (any, acc -> acc)) :: acc
-  def foldl(l, acc, f) do
-  end
+  def foldl([], acc, _f), do: acc
+  def foldl([head | tail], acc, f), do: foldl(tail, f.(head, acc), f)
 
   @spec foldr(list, acc, (any, acc -> acc)) :: acc
-  def foldr(l, acc, f) do
-  end
+  def foldr([], acc, _f), do: acc
+  def foldr([head | tail], acc, f), do: f.(head, foldr(tail, acc, f))
 
   @spec append(list, list) :: list
-  def append(a, b) do
-  end
+  def append(a, b), do: concat([a, b])
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-  end
+  def concat(ll), do: concat(ll, [])
+  defp concat([], result), do: reverse(result)
+  defp concat([[] | ll], result), do: concat(ll, result)
+  defp concat([[head | tail] | ll], result), do: concat([tail | ll], [head | result])
 end
