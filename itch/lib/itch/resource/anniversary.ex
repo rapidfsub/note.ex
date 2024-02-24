aff459c3368b610009f0275427b070dfcde6ffcd
Minsub Kim <rapidfsub@gmail.com>
2024-02-24 15:03:40 +0900

    remind anniversaries

--- itch/lib/itch/resource/anniversary.ex
+++ itch/lib/itch/resource/anniversary.ex
@@ -20,11 +20,24 @@ defmodule Itch.Resource.Anniversary do
   end
 
   actions do
-    defaults [:create]
+    defaults [:create, :read]
 
     create :upsert do
       upsert? true
       upsert_identity :unique_name
     end
+
+    read :list_by_type do
+      argument :calendar_kind, Itch.Enum.CalendarKind
+      filter expr(is_nil(^arg(:calendar_kind)) or calendar_kind == ^arg(:calendar_kind))
+    end
+  end
+
+  code_interface do
+    define_for Itch.Api
+    define :create
+    define :read
+    define :upsert
+    define :list_by_type, args: [:calendar_kind]
   end
 end
