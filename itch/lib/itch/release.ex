9979a3a3274f79c30ec4e9b5c2c9fbe2ea93efef
Minsub Kim <rapidfsub@gmail.com>
2024-02-24 15:23:16 +0900

    comment out migration

--- itch/lib/itch/release.ex
+++ itch/lib/itch/release.ex
@@ -8,9 +8,9 @@ defmodule Itch.Release do
   def migrate do
     load_app()
 
-    for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
-    end
+    # for repo <- repos() do
+    #   {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    # end
 
     Mix.Task.run("ecto.reset")
   end
