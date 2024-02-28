dd27c817462dd121b4b5094584f2f21a0cebd624
Minsub Kim <rapidfsub@gmail.com>
2024-02-29 03:01:04 +0900

    fix deps version constraints

--- bed/mix.exs
+++ bed/mix.exs
@@ -32,17 +32,17 @@ defmodule Bed.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.11"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.10"},
+      {:phoenix, ">= 0.0.0"},
+      {:phoenix_ecto, ">= 0.0.0"},
+      {:ecto_sql, ">= 0.0.0"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 4.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.20.2"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
+      {:phoenix_html, ">= 0.0.0"},
+      {:phoenix_live_reload, ">= 0.0.0", only: :dev},
+      {:phoenix_live_view, ">= 0.0.0"},
+      {:floki, ">= 0.0.0", only: :test},
+      {:phoenix_live_dashboard, ">= 0.0.0"},
+      {:esbuild, ">= 0.0.0", runtime: Mix.env() == :dev},
+      {:tailwind, ">= 0.0.0", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.1.1",
@@ -50,14 +50,14 @@ defmodule Bed.MixProject do
        app: false,
        compile: false,
        depth: 1},
-      {:swoosh, "~> 1.5"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
-      {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:swoosh, ">= 0.0.0"},
+      {:finch, ">= 0.0.0"},
+      {:telemetry_metrics, ">= 0.0.0"},
+      {:telemetry_poller, ">= 0.0.0"},
+      {:gettext, ">= 0.0.0"},
+      {:jason, ">= 0.0.0"},
+      {:dns_cluster, ">= 0.0.0"},
+      {:bandit, ">= 0.0.0"}
     ]
   end
 
