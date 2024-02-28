9eafff8684831832f13b4210bf02df79162bf2be
Minsub Kim <rapidfsub@gmail.com>
2024-02-29 03:25:55 +0900

    fix regen migration for rollback all

--- bed/regen.sh
+++ bed/regen.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# Get count of untracked migrations
-N_MIGRATIONS=$(git ls-files --others priv/repo/migrations | wc -l)
-
-# Rollback untracked migrations
-mix ecto.rollback -n $N_MIGRATIONS
-
-# Delete untracked migrations and snapshots
-git ls-files --others priv/repo/migrations | xargs rm
-git ls-files --others priv/resource_snapshots | xargs rm
+# Delete migrations and snapshots
+ls priv/repo/migrations/[^.]* | xargs rm
+rm -rf priv/resource_snapshots
 
 # Regenerate migrations
 mix ash_postgres.generate_migrations
 
-# Run migrations if flag
-if echo $* | grep -e "-m" -q; then
-  mix ecto.migrate
-fi
+# Reset database
+mix ecto.reset
