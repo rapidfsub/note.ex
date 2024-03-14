ddb89463e80d0285878f8b2e9d37ab2289930626
Minsub Kim <rapidfsub@gmail.com>
2024-03-14 14:26:42 +0900

    solve freelancer rates

--- exercism/elixir/freelancer-rates/lib/freelancer_rates.ex
+++ exercism/elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,21 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    before_discount * (100.0 - discount) / 100.0
+  end
+
+  defp discounted_daily_rate(hourly_rate, discount) do
+    hourly_rate |> daily_rate() |> apply_discount(discount)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    (discounted_daily_rate(hourly_rate, discount) * 22.0) |> ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    (budget / discounted_daily_rate(hourly_rate, discount)) |> Float.floor(1)
   end
 end
