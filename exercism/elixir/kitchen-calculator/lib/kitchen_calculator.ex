d23d0304a1443b03081fbee768ce623695250e7d
Minsub Kim <rapidfsub@gmail.com>
2024-03-14 22:58:53 +0900

    solve kitchen calculator

--- exercism/elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ exercism/elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,17 +1,23 @@
 defmodule KitchenCalculator do
   def get_volume(volume_pair) do
-    # Please implement the get_volume/1 function
+    volume_pair |> elem(1)
   end
 
-  def to_milliliter(volume_pair) do
-    # Please implement the to_milliliter/1 functions
+  def to_milliliter({unit, volume}) do
+    {:milliliter, conversion_factor(unit) * volume}
   end
+
+  defp conversion_factor(:milliliter), do: 1
+  defp conversion_factor(:cup), do: 240
+  defp conversion_factor(:fluid_ounce), do: 30
+  defp conversion_factor(:teaspoon), do: 5
+  defp conversion_factor(:tablespoon), do: 15
 
   def from_milliliter(volume_pair, unit) do
-    # Please implement the from_milliliter/2 functions
+    {unit, get_volume(volume_pair) / conversion_factor(unit)}
   end
 
   def convert(volume_pair, unit) do
-    # Please implement the convert/2 function
+    volume_pair |> to_milliliter() |> from_milliliter(unit)
   end
 end
